2b0f855e3c5d44eea2966538d9c5e73e65859bbc
hitsuji24 <2xx4xx.mole.melajis@gmail.com>
2023-11-21 16:49:52 +0900

    為替レートの反映を試みた

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
+
 // DOMがロードされた後、イベントリスナーが設定され、国の選択やじゃんけんの手の選択が可能になる
 document.addEventListener('DOMContentLoaded', function () {
     let playerMoney = 10000; // プレイヤーの初期所持金
     let roundCount = 0; // 現在のラウンド数
+    let currentPlayerCountry = 'JPN'; // 初期国は日本
+    const exchangeRates = {
+        USA: { JPN: 125, BRA: 5, CHN: 6.67, IDN: 143, AUS: 0.714 },
+        JPN: { USA: 0.008, BRA: 0.04, CHN: 0.0533, IDN: 1.143, AUS: 0.0057 },
+        BRA: { USA: 0.2, JPN: 25, CHN: 1.33, IDN: 28.57, AUS: 0.14 },
+        CHN: { USA: 0.15, JPN: 18.75, BRA: 0.75, IDN: 21.43, AUS: 0.107 },
+        IDN: { USA: 0.007, JPN: 0.875, BRA: 0.035, CHN: 0.0467, AUS: 0.005 },
+        AUS: { USA: 1.4, JPN: 175, BRA: 7, CHN: 9.33, IDN: 200 }
+    };
+
+    fillExchangeRatesTable(); // 為替レート表を表示
 
     // countryオブジェクトに各国のじゃんけんの手と倍率を定義 
     const country = {
-        japan: {
+        JPN: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -15,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
             lossMultiplier: 1.5 // 敗北時のペナルティ倍率
         },
 
-        usa: {
+        USA: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -25,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
             lossMultiplier: 5 // 敗北時のペナルティ倍率
         },
 
-        brazil: {
+        BRA: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -35,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
             lossMultiplier: 5 // 敗北時のペナルティ倍率
         },
 
-        china: {
+        CHN: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -45,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
             lossMultiplier: 5 // 敗北時のペナルティ倍率
         },
 
-        indonesia: {
+        IDN: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -55,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
             lossMultiplier: 5 // 敗北時のペナルティ倍率
         },
 
-        australia: {
+        AUS: {
             hands: [
                 { name: 'rock', image: '/img/rock.png' },
                 { name: 'scissors', image: '/img/scissors.png' },
@@ -71,7 +83,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const countryImages = document.querySelectorAll('.country img');
     countryImages.forEach(img => {
         img.addEventListener('click', function () {
-            const countryName = this.alt.toLowerCase(); // 国名を取得
+            const countryName = this.alt // 国名を取得
+            // 【備忘録】国名を大文字のコードにしたので.toLowerCase()が悪さしていた
             displayHands(country[countryName]);
         });
     });
@@ -150,4 +163,23 @@ document.addEventListener('DOMContentLoaded', function () {
         row.insertCell(2).textContent = result;
         row.insertCell(3).textContent = playerMoney;
     }
+
+    // 情報エリアに為替レートを表示
+    function fillExchangeRatesTable() {
+        const tableBody = document.getElementById('exchangeRatesTable').getElementsByTagName('tbody')[0];
+        tableBody.innerHTML = ''; // テーブルの中身をクリア
+
+        for (const [fromCurrency, rates] of Object.entries(exchangeRates)) {
+            const row = tableBody.insertRow();
+            row.insertCell().textContent = fromCurrency;
+
+            for (const toCurrency of Object.keys(exchangeRates)) {
+                const rate = rates[toCurrency] || '-';
+                row.insertCell().textContent = rate;
+            }
+        }
+    }
 });
+
+
+
